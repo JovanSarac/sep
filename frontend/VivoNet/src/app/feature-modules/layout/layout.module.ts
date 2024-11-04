77cdc84f82c1dba1c72950a05a8d88ec590039da
JovanSarac <jovan_bn@outlook.com>
2024-11-04 23:25:36 +0100

    feat:Added footer for webshop

--- frontend/VivoNet/src/app/feature-modules/layout/layout.module.ts
+++ frontend/VivoNet/src/app/feature-modules/layout/layout.module.ts
@@ -2,19 +2,22 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar/navbar.component';
 import { HomeComponent } from './home/home.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 
 @NgModule({
   declarations: [
     NavbarComponent,
-    HomeComponent
+    HomeComponent,
+    FooterComponent
   ],
   imports: [
     CommonModule
   ],
   exports: [
-    NavbarComponent
+    NavbarComponent,
+    FooterComponent
   ],
 })
 export class LayoutModule { }
