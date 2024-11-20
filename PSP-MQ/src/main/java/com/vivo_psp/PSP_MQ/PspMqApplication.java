93143a7a4ad64ab6732816b96410d20f32a6968b
Nevena Gligorov <gligorov.nevena@gmail.com>
2024-11-20 13:15:33 +0100

    feat: mq producer side

---
+++ PSP-MQ/src/main/java/com/vivo_psp/PSP_MQ/PspMqApplication.java
@@ -0,0 +1,13 @@
+package com.vivo_psp.PSP_MQ;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PspMqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PspMqApplication.class, args);
+	}
+
+}
---
+++ PSP-MQ/src/test/java/com/vivo_psp/PSP_MQ/PspMqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vivo_psp.PSP_MQ;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PspMqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
