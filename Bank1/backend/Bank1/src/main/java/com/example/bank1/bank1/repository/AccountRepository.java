6041c27e54e736f2ee0b1ebb986e22218984b238
leopoldina01 <leopoldinica123@gmail.com>
2024-11-26 13:26:00 +0100

    fix: Fix annotation

--- Bank1/backend/Bank1/src/main/java/com/example/bank1/bank1/repository/AccountRepository.java
+++ Bank1/backend/Bank1/src/main/java/com/example/bank1/bank1/repository/AccountRepository.java
@@ -4,9 +4,14 @@ import com.example.bank1.bank1.model.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface AccountRepository extends JpaRepository<Account, Long> {
 
     Account getAccountByAccountNumber(String accountNumber);
     Account getAccountByPAN(Long PAN);
+
+    Optional<Account> findByAccountNumber(String accountNumber);
+    Optional<Account> findByPAN(Long PAN);
 }
--- Bank1/backend/Bank1/src/main/java/com/example/bank1/bank1/repository/TransactionRepository.java
+++ Bank1/backend/Bank1/src/main/java/com/example/bank1/bank1/repository/TransactionRepository.java
@@ -2,6 +2,8 @@ package com.example.bank1.bank1.repository;
 
 import com.example.bank1.bank1.model.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,5 +12,7 @@ import java.util.UUID;
 @Repository
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
     List<Transaction> findAllBySourceAccountNumber(String sourceAccountNumber);
-    Transaction findByAcquirerOrderIdAAndIssuerOrderId(UUID acquirerOrderId, UUID issuerOrderId);
+    @Query("SELECT t FROM Transaction t WHERE t.acquirerOrderId = :acquirerOrderId AND t.issuerOrderId = :issuerOrderId")
+    Transaction findByAcquirerOrderIdAAndIssuerOrderId(@Param("acquirerOrderId") UUID acquirerOrderId,
+                                                       @Param("issuerOrderId") UUID issuerOrderId);
 }
