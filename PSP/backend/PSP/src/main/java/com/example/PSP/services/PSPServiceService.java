d708fcff6f4f9f831c94e74049c995d1d7bf6f09
Nevena Gligorov <gligorov.nevena@gmail.com>
2025-08-21 15:42:31 +0200

    feat: service discovery for PSP

--- PSP/backend/PSP/src/main/java/com/example/PSP/services/PSPServiceService.java
+++ PSP/backend/PSP/src/main/java/com/example/PSP/services/PSPServiceService.java
@@ -5,14 +5,18 @@ import com.example.PSP.repositories.PSPServiceRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class PSPServiceService {
     @Autowired
     private final PSPServiceRepository _pspServiceRepository;
+    @Autowired
+    private DiscoveryClient discoveryClient;
 
     private static final Logger logger = LoggerFactory.getLogger(PSPServiceService.class);
 
@@ -22,6 +26,22 @@ public class PSPServiceService {
 
     public List<PSPService> getActiveServices() {
         logger.info("Retrieving all active PSP services");
-        return _pspServiceRepository.findAllActivePSPServices();
+        List<String> registeredServices = discoveryClient.getServices()
+                .stream()
+                .filter(s -> !s.toUpperCase().equals("PSP") && !s.toLowerCase().equals("consul"))
+                .toList();
+        List<PSPService> allPspServices = _pspServiceRepository.findAllActivePSPServices();
+
+        List<PSPService> activePspServices = new ArrayList<>();
+
+        for(var service : allPspServices){
+            for(var registeredService : registeredServices){
+                if(service.getName().toLowerCase().contains(registeredService.toLowerCase())){
+                    activePspServices.add(service);
+                }
+            }
+        }
+
+        return activePspServices;
     }
 }
