bf00ef1dbb111cb3584ce6f0ae055b50a33b6f67
Leopoldina Djanic <leopoldinica123@gmail.com>
2025-09-19 16:23:28 +0200

    feat: add sso to bank1

--- Bank1/frontend/Bank1/src/app/app-routing.module.ts
+++ Bank1/frontend/Bank1/src/app/app-routing.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { UserDataComponent } from './feature-modules/layout/user-data/user-data.component';
 import { QrcodeComponent } from './feature-modules/layout/qrcode/qrcode.component';
+import { AuthGuard } from '../app/infrastracture/auth/guard/auth.guard';
 
 const routes: Routes = [
-  {path: '', component: UserDataComponent},
-  {path: 'qrCode', component: QrcodeComponent}
+  {path: '', component: UserDataComponent, canActivate: [AuthGuard]},
+  {path: 'qrCode', component: QrcodeComponent, canActivate: [AuthGuard]}
 ];
 
 @NgModule({
